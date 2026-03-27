9d2e0ec6fc0635188ac77245c6bfd7e1eea50f56
Matteo <matteodetoma04@gmail.com>
2026-03-27 17:43:19 +0100

    Revert "Ajout d'un dossier Plot Python"
    This reverts commit cc4a445d1563bd32d9fabd37a366bee136e5d1a4.

--- plot_menu.cpp
+++ plot_menu.cpp
@@ -35,13 +35,13 @@ int main() {
         switch (choix) {
             case 1:
                 cout << "\nExecution de plot_simulation.py..." << endl;
-                commande = "python \"Plot Python/plot_simulation.py\"";
+                commande = "python plot_simulation.py";
                 system(commande.c_str());
                 break;
                 
             case 2:
                 cout << "\nExecution de plot_rescue_comparison.py..." << endl;
-                commande = "python \"Plot Python/plot_rescue_comparison.py\"";
+                commande = "python plot_rescue_comparison.py";
                 system(commande.c_str());
                 break;
                 
@@ -49,30 +49,30 @@ int main() {
 
             case 3:
                 cout << "\nExecution de plot_aerodynamic_coefficients.py..." << endl;
-                commande = "python \"Plot Python/plot_aerodynamic_coefficients.py\"";
+                commande = "python plot_aerodynamic_coefficients.py";
                 system(commande.c_str());
                 break;
 
             case 4:
                 cout << "\nExecution de plot_CL_vs_alpha.py..." << endl;
-                commande = "python \"Plot Python/plot_CL_vs_alpha.py\"";
+                commande = "python plot_CL_vs_alpha.py";
                 system(commande.c_str());
                 break;
             case 5:
                 cout << "\nExecution de Phugoide.py..." << endl;
-                commande = "python \"Plot Python/Phugoide.py\"";
+                commande = "python Phugoide.py";
                 system(commande.c_str());
                 break;
 
             case 6:
                 cout << "\nExecution de plot_min_rescue_altitude.py..." << endl;
-                commande = "python \"Plot Python/plot_min_rescue_altitude.py\"";
+                commande = "python plot_min_rescue_altitude.py";
                 system(commande.c_str());
                 break;
 
             case 7:
                 cout << "\nExecution de plot_heatmap_crash.py..." << endl;
-                commande = "python \"Plot Python/plot_heatmap_crash.py\"";
+                commande = "python plot_heatmap_crash.py";
                 system(commande.c_str());
                 break;
                 
